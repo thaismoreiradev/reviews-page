0f34f00b15d26967396c93a48a63739dff63f35f
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-10-07 21:52:39 -0300

    more steps

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+
+
+const reviews = [
+    {
+        id: 1,
+        name: "Samara",
+        job: "teacher",
+        img: ,
+        text:
+        "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
+
+    },
+        {
+        id: 2,
+        name: "Molly",
+        job: "Design",
+        img: ,
+        text:
+        "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
+    }
+]
+
+const img = document.querySelector("#person-img")
+const author = document.querySelector("#author")
+const job = document.querySelector("#job")
+const info = document.querySelector("#info")
+
+const prevBtn = document.querySelector("#prev-btn")
+const nextBtn = document.querySelector("#next-btn")
