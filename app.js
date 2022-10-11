f245f7cd0b60a692d22a45ed7cdad76d6322e1d2
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-10-10 22:17:35 -0300

    js base finished, css started

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 
 
-
+// reviews data
 const reviews = [
     {
         id: 1,
         name: "Samara",
         job: "teacher",
-        img: ,
+        img: "",
         text:
         "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
 
@@ -16,12 +16,13 @@ const reviews = [
         id: 2,
         name: "Molly",
         job: "Design",
-        img: ,
+        img: "",
         text:
         "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
     }
 ]
 
+//  items
 const img = document.querySelector("#person-img")
 const author = document.querySelector("#author")
 const job = document.querySelector("#job")
@@ -29,3 +30,37 @@ const info = document.querySelector("#info")
 
 const prevBtn = document.querySelector("#prev-btn")
 const nextBtn = document.querySelector("#next-btn")
+
+// starting item
+let currentItem = 0
+
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson();
+})
+
+// show person based on item
+const showPerson = () => {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text
+}
+
+//  show next person 
+nextBtn.addEventListener("click", function () {
+    currentItem++
+    if(currentItem > reviews.length -1){
+        currentItem = 0;
+    }
+    showPerson();
+})
+
+// show previous person
+prevBtn.addEventListener("click", function() {
+    currentItem--
+    if(currentItem < 0){
+        currentItem = reviews.length -1
+    }
+    showPerson();
+})
