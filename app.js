5e7bb676c59a2cfedc555d1e8aab6573a873ebeb
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-10-11 22:18:52 -0300

    css  details

--- app.js
+++ app.js
@@ -5,20 +5,27 @@
 const reviews = [
     {
         id: 1,
-        name: "Samara",
-        job: "teacher",
-        img: "",
+        name: "Winifred",
+        job: "Teacher",
+        img: "./img/winifred.png",
         text:
-        "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
+        "This product is very thick and gloopy and takes a few minutes to seek in and even then your face will feel sticky to the touch. I won’t buy it again. I’m not going to return it because I will use it up - not just on my face, but neck and chest area, and the top of my hands. Oh well…",
 
     },
-        {
+    {
         id: 2,
-        name: "Molly",
+        name: "Sarah",
         job: "Design",
-        img: "",
+        img: "./img/sarah.png",
         text:
-        "kajaaaaaajjjjjjoaoakoakoa ajajajia anhaahuuhs fhghguug eeuduudu ajaoaoka hahuaha ahajjiaieognnvnnvnvnvnv",
+        "I got a free sample of Drops of Youth and loved it. Made my skin feel so soft and silky so I bought this one which was supposed to be the same but re branded. Definitely not the same, leaves my skin feeling tight and sticky and like I need to moisturise afterwards. Wish I didn’t spend the money on this!",
+    },
+    {
+        id: 3,
+        name: "Mary",
+        job: "Engineer",
+        img: "./img/mary.png",
+        text: "I got a free sample of the original Drops of Youth and absolutely loved it. So when I could afford it I bought a large bottle but the name had changed to edelweiss. It’s not quite same, still nice but not the game changer it was. I liked applying some Drops of Youth lol",
     }
 ]
 
